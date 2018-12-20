65db2b3f38d32f166b1f40595f2490b876e9f62e
YingkaiFu <Yingkai.Fu@hotmail.com>
2018-12-20 22:20:52 +0800

    chore: 界面小问题修改

--- src/main/java/cn/edu/ncu/jiluan/bbs/entity/PostEntity.java
+++ src/main/java/cn/edu/ncu/jiluan/bbs/entity/PostEntity.java
@@ -13,9 +13,9 @@ public class PostEntity {
     private int views;
     private int likes;
     private int replies;
-    private Timestamp lastreply;
     private Integer plateId;
     private Integer userId;
+    private Timestamp lastReply;
 
     @Id
     @Column(name = "post_id", nullable = false)
@@ -87,16 +87,6 @@ public class PostEntity {
         this.replies = replies;
     }
 
-    @Basic
-    @Column(name = "lastreply", nullable = false)
-    public Timestamp getLastreply() {
-        return lastreply;
-    }
-
-    public void setLastreply(Timestamp lastreply) {
-        this.lastreply = lastreply;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -111,7 +101,6 @@ public class PostEntity {
         if (postTopic != null ? !postTopic.equals(that.postTopic) : that.postTopic != null) return false;
         if (postCont != null ? !postCont.equals(that.postCont) : that.postCont != null) return false;
         if (postTime != null ? !postTime.equals(that.postTime) : that.postTime != null) return false;
-        if (lastreply != null ? !lastreply.equals(that.lastreply) : that.lastreply != null) return false;
 
         return true;
     }
@@ -125,7 +114,6 @@ public class PostEntity {
         result = 31 * result + views;
         result = 31 * result + likes;
         result = 31 * result + replies;
-        result = 31 * result + (lastreply != null ? lastreply.hashCode() : 0);
         return result;
     }
 
@@ -147,5 +135,15 @@ public class PostEntity {
 
     public void setUserId(Integer userId) {
         this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "last_reply", nullable = false)
+    public Timestamp getLastReply() {
+        return lastReply;
+    }
+
+    public void setLastReply(Timestamp lastReply) {
+        this.lastReply = lastReply;
     }
 }
