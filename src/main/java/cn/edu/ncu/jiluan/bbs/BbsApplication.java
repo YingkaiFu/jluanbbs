5ef83c717f27ea9997a670580c7b20417fa4777e
YingkaiFu <Yingkai.Fu@hotmail.com>
2018-12-20 15:22:56 +0800

    Initial commit

---
+++ src/main/java/cn/edu/ncu/jiluan/bbs/BbsApplication.java
@@ -0,0 +1,14 @@
+package cn.edu.ncu.jiluan.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/cn/edu/ncu/jiluan/bbs/BbsApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.edu.ncu.jiluan.bbs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BbsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
