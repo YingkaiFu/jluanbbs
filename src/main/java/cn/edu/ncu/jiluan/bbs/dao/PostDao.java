645e36e081fb76b2fed048a0ab8c52f2debdf690
heyhwi <huweichu@gmail.com>
2018-12-22 19:18:15 +0800

    feat:按板块浏览后端

--- src/main/java/cn/edu/ncu/jiluan/bbs/dao/PostDao.java
+++ src/main/java/cn/edu/ncu/jiluan/bbs/dao/PostDao.java
@@ -3,8 +3,12 @@ package cn.edu.ncu.jiluan.bbs.dao;
 import cn.edu.ncu.jiluan.bbs.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by krito on 2018/12/20
  */
 public interface PostDao extends JpaRepository<PostEntity, Integer> {
+
+    List<PostEntity> findPostEntitiesByPlateId(Integer plateId);
 }
