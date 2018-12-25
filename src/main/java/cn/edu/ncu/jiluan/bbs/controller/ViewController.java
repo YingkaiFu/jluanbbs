ddaf98cbe49cfa0d7a77e96b31a40a6e964d6378
krito <krito5898@gmail.com>
2018-12-26 06:39:12 +0800

    fix: 修复未登录存在回复框的问题
    懒得睡了把该改的都改了
    我以后commit一次就说一次：
    **JPA的级联查询就是弟弟啦**

--- src/main/java/cn/edu/ncu/jiluan/bbs/controller/ViewController.java
+++ src/main/java/cn/edu/ncu/jiluan/bbs/controller/ViewController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping(value = "")
 public class ViewController {
@@ -20,9 +23,9 @@ public class ViewController {
     private ReplyService replyService;
 
     @RequestMapping(value = "/post/{postId}",method = RequestMethod.GET)
-    public String GetPostContent(@PathVariable Integer postId, Model model){
+    public String GetPostContent(@PathVariable Integer postId, Model model, HttpServletRequest request){
         PostEntity post = postService.findPostEntityByPostId(postId, Byte.valueOf("0"));
-
+        HttpSession session = request.getSession();
         model.addAttribute("post", post);
         model.addAttribute("replyList",replyService.findReplyEntitiesByPostId(postId));
         model.addAttribute("reply",new ReplyEntity());
